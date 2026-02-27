a6272fb575f51359c08f347f1f36ddf40818ab6e
rafihabibi <68018633+rafihabibi@users.noreply.github.com>
2026-02-27 09:19:53 +0700

    Update No.3KonversiCtoF.js

--- No.3KonversiCtoF.js
+++ No.3KonversiCtoF.js
@@ -1,10 +1,10 @@
 function konversiCF(celcius){
   let fahrenheit = 0;
-  fahrenheit = celcius * 1.8 + 32
+  fahrenheit = celcius * 1.8 + 32;
   return fahrenheit;
   
 }
 
-let suhuC = 26;
-let konversi = `Setelah di Konversi C ke F: ${konversiCF(suhuC)}`
-console.log(konversi)
+let suhuC = 29;
+let konversi = `Setelah di Konversi C ke F: ${Math.floor(konversiCF(suhuC))}°`;
+console.log(konversi);
